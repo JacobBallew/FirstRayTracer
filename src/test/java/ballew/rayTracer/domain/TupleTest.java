f70da4531f077f0cd430eeb74a4db391e18dc6d2
Jacob Ballew <jballew7@gmail.com>
2020-04-25 20:49:14 -0400

    Commit

---
+++ src/test/java/ballew/rayTracer/domain/TupleTest.java
@@ -0,0 +1,17 @@
+package ballew.rayTracer.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TupleTest {
+
+    @Test
+    public void isPoint() {
+        Tuple p = new Tuple(1,2,3,0);
+        Assert.assertEquals(true, p.isPoint());
+    }
+
+
+}
